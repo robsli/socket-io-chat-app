be951cef99c645d97c43cfb3fb61c800a568de42
robsli <robbieli121.zy@gmail.com>
2018-10-08 23:01:58 -0500

    connect to socket server in prod

--- src/services/chat-data-service.js
+++ src/services/chat-data-service.js
@@ -14,28 +14,48 @@ function ChatDataService({
     NEW_MESSAGE: 'NEW MESSAGE',
     NEW_NAME: 'NEW NAME'
   }
-  const socketServer = 'http://192.168.0.40:8080'
-  const socket = io(socketServer)
 
-  socket.on(actions.CONNECTED, () => {
-    onReceiveSocketID(socket.id)
-  })
+  const socketServer = process.env.NODE_ENV === 'development'
+  ? 'http://localhost:8080'
+  : 'https://socketio-chat-app-staging.herokuapp.com'
 
-  socket.on(actions.FAILED_MESSAGE, () => {
-    onFailedMessage()
-  })
+  let socket = process.env.NODE_ENV === 'development' ? io(socketServer) : null
+  process.env.NODE_ENV === 'production' &&
+    fetch(socketServer, {
+      credentials: 'include',
+      method: 'GET'
+    })
+    .then((response) => {
+      socket = io(`${socketServer}:${response}`)
+      defineSocketListeners(socket)
+    })
+    .catch((error) => {
+      console.log('There was an error connecting to the server: ' + error)
+    })
 
-  socket.on(actions.LOAD_MESSAGE, (messages) => {
-    onLoadMessages(messages)
-  })
+  process.env.NODE_ENV === 'development' && defineSocketListeners(socket)
 
-  socket.on(actions.NEW_MESSAGE, (message) => {
-    onNewMessage(message)
-  })
-
-  socket.on(actions.NEW_NAME, (newName) => {
-    onNewName(newName)
-  })
+  function defineSocketListeners(socket) {
+    socket.on(actions.CONNECTED, () => {
+      onReceiveSocketID(socket.id)
+    })
+  
+    socket.on(actions.FAILED_MESSAGE, () => {
+      onFailedMessage()
+    })
+  
+    socket.on(actions.LOAD_MESSAGE, (messages) => {
+      onLoadMessages(messages)
+    })
+  
+    socket.on(actions.NEW_MESSAGE, (message) => {
+      onNewMessage(message)
+    })
+  
+    socket.on(actions.NEW_NAME, (newName) => {
+      onNewName(newName)
+    })
+  }
 
   function getMessages() {
     socket.emit(actions.LOAD_MESSAGE)
